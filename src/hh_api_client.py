2b4d2dcaac69d02b08f621be49df8a7960e5a56c
georgii <goqwertys@gmail.com>
2024-09-11 02:52:17 +0400

    HHAPIClient done

--- src/hh_api_client.py
+++ src/hh_api_client.py
@@ -5,20 +5,26 @@ from src.api_client import APIClient
 class HHAPIClient(APIClient):
     """ Client for fetching data from hh.ru public API """
     BASE_URL = 'https://api.hh.ru/vacancies'
+    EMPLOYER_URL = 'https://api.hh.ru/employers'
 
-    def __init__(self, employer_id):
+    def __init__(self):
         self.headers = {'User-Agent': 'HH-User-Agent'}
         self.params = {
-            'employer_id': employer_id,
-            'per_page': 100  # Number of vacancies on one page
+            'per_page': 100,  # Number of vacancies on one page
+            'page': 0  # Initial page
         }
-
         self.vacancies = []
 
-    def load_vacancies_by_emp_id(self, employer_id):
-        """ Loads vacancies by employer_id """
-        # TODO employer_id validation
+    def load_vacancy_by_emp_id(self, employer_id):
+        """ Loads vacancies by a single employer_id """
+        # Validation
+        employer_id = self.valid_id(employer_id)
+        self.check_existence(employer_id)
+
+        # Requesting data
         self.params['employer_id'] = employer_id
+        self.params['page'] = 0  # Reset page to 0
+
         while True:
             try:
                 response = requests.get(self.BASE_URL, headers=self.headers, params=self.params)
@@ -31,6 +37,32 @@ class HHAPIClient(APIClient):
                 self.params['page'] += 1
             except requests.RequestException as e:
                 print(f'An error has occurred: {e}')
+                break
+
+    def load_vacancies_by_emp_ids(self, emp_ids: list[int]):
+        """ Loads vacancies by a list of employer_ids """
+        for employer_id in emp_ids:
+            self.load_vacancy_by_emp_id(employer_id)
+
+    @staticmethod
+    def valid_id(employer_id):
+        """ Validates employer id """
+        if isinstance(employer_id, int) and employer_id > 0:
+            return employer_id
+        raise ValueError('Invalid employer ID')
+
+    @staticmethod
+    def check_existence(employer_id):
+        """ Checks if employer_id exists on hh.ru """
+        url = f"{HHAPIClient.EMPLOYER_URL}/{employer_id}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            return True
+        elif response.status_code == 404:
+            raise ValueError("Employer ID does not exist")
+        else:
+            raise ValueError("Failed to check employer ID")
+
 
     def get_info(self):
         return self.vacancies
