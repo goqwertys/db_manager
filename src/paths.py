37544cbeea1c1135aec6f07d91ff610245550282
georgii <goqwertys@gmail.com>
2024-09-11 04:15:47 +0400

    paths.root_join added

---
+++ src/paths.py
@@ -0,0 +1,7 @@
+import os
+
+def root_join(*parts):
+    """ A function that returns the path to the file specified from the root """
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    full_path = os.path.join(base_dir, '..', *parts)
+    return full_path
