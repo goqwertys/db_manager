7da883b50cd7d782771ea45ab20c76bd0ea88a38
georgii <goqwertys@gmail.com>
2024-09-12 02:53:57 +0400

    DBManager(ABSManager) added unfinished

---
+++ src/db_manager.py
@@ -0,0 +1,39 @@
+import psycopg2
+import logging
+
+from src.abs_manager import ABSManager
+
+from src.paths import root_join
+from src.config import LOG_LEVEL
+
+# Logger setup
+logger = logging.getLogger(__name__)
+logger.setLevel(LOG_LEVEL)
+log_path = root_join('logs', f'{__name__}.log')
+fh = logging.FileHandler(log_path, mode='w')
+formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+class DBManager(ABSManager):
+    """ Database manager """
+
+    def __init__(self, params=None):
+        """ Connects to Database and creates cursor """
+        self.conn = psycopg2.connect(dbname='hh_db', **params)
+        self.cur = self.conn.cursor()
+
+    def get_companies_and_vacancies_count(self):
+        """ Returns a list of all companies and the number of vacancies for each company"""
+
+    def get_all_vacancies(self):
+        """ Returns a list of all vacancies with the company name, job title and salary, and a link to the vacancy."""
+
+    def get_avg_salary(self):
+        """ Returns average salary by vacancies """
+
+    def get_vacancies_with_higher_salary(self):
+        """ Returns a list of all vacancies where the salary is higher than the average for all vacancies """
+
+    def get_vacancies_with_keyword(self, keyword: str):
+        """ Returns a list of all vacancies whose titles contain the words passed to the method, for example python """
