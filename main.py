c0760200acb12eed8850fec116a9f14e317b6119
georgii <goqwertys@gmail.com>
2024-09-19 20:10:57 +0400

    main logic done/ program it seems that program working stably

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import json
 import logging
-import os
-from pathlib import Path
 
-from src.config import LOG_LEVEL
+from src.config import LOG_LEVEL, TEXT_WIDTH
+from src.db_manager import DBManager
 from src.hh_api_client import HHAPIClient
 from src.paths import root_join
-from src.utils import read_employers_list, read_db_config, create_database, save_to_database
+from src.utils import read_employers_list, read_db_config, create_database, save_to_database, fixed_width
 
 # Logger setup
 logger = logging.getLogger(__name__)
@@ -16,6 +14,15 @@ fh = logging.FileHandler(log_path, mode='w')
 formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
 fh.setFormatter(formatter)
 logger.addHandler(fh)
+
+commands = [
+    ("EXT", "Exit the program"),
+    ("CVCOUNT", "Get a list of all companies and the number of vacancies for each company"),
+    ("GETALL", "Get a list of all vacancies with the company name, job title and salary and a link to the vacancy"),
+    ("AVGSAL", "Get average salary for vacancies"),
+    ("HIAVG", "Get a list of all vacancies where the salary is higher than the average for all vacancies"),
+    ("KEYW", "Get a list of all vacancies whose titles contain the words passed to the method, for example python")
+]
 
 def main() -> bool:
     """ Main function """
@@ -61,51 +68,251 @@ def main() -> bool:
         logger.info('Inserting data to Database...')
         save_to_database(areas, employers, vacancies, db_name, params)
 
-        # TEMP
-        # data = hh_client.get_info()
-
-        # dumping temp data
-        # file_path = root_join('data', 'tmp_json.json')
-
-        # if not os.path.exists(file_path):
-        #     Path(file_path).touch()
-        # with open(file_path, 'w', encoding='utf-8') as f:
-        #     json.dump(data, f, indent=4, ensure_ascii=False)
-
+        # Creating db_manager
+        logger.info('Creating DBManager instance')
+        db_manager = DBManager(db_name, params)
 
     except Exception as e:
         logger.error(f'An error has occurred: {e}')
         return False
 
     while True:
-        print("""Please enter the following characters to:
-        'EXT' : Exit the program
-        'CVCOUNT' : Get a list of all companies and the number of vacancies for each company
-        'GETALL' : Get a list of all vacancies with the company name, job title and salary and a link to the vacancy
-        'AVGSAL' : Get average salary for vacancies
-        'HIAVG' : Get a list of all vacancies where the salary is higher than the average for all vacancies
-        'KEYW' : Get a list of all vacancies whose titles contain the words passed to the method, for example python
-        """)
+        print("Please enter the following characters to:")
+        for command in commands:
+            print(fixed_width(
+                f"'{command[0]}'",
+                f"{command[1]}",
+                '-',
+                TEXT_WIDTH
+            ))
         user_input = input()
 
+        # Exit program
         if user_input.lower() == 'ext':
             logger.info('Terminating the program')
             return True
 
+        # Vacancies count
         elif user_input.lower() == 'cvcount':
-            pass
+            vacancies_count = db_manager.get_companies_and_vacancies_count()
+            print('~' * TEXT_WIDTH)
+            print('NUMBER OF VACANCIES FOR EACH EMPLOYER:'.center(TEXT_WIDTH, '~'))
+            print('~' * TEXT_WIDTH)
+            for item in vacancies_count:
+                print('=' * TEXT_WIDTH)
 
+                # print(item)
+
+                print(fixed_width(
+                    f'Employer: {item[0]}',
+                    f'Vacancies: {item[1]}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                print('=' * TEXT_WIDTH)
+
+        # Getting all
         elif user_input.lower() == 'getall':
-            pass
+            all_vacancies = db_manager.get_all_vacancies()
+            print('~' * TEXT_WIDTH)
+            print('ALL FOUND VACANCIES:'.center(TEXT_WIDTH, '~'))
+            print('~' * TEXT_WIDTH)
+            for item in all_vacancies:
+                print('=' * TEXT_WIDTH)
+
+                # TEMP
+                # print(item)
+
+                # ID [0]
+                print(fixed_width(
+                    'ID:',
+                    f'{item[0] if item[0] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Employer [1]
+                print(fixed_width(
+                    'Company:',
+                    f'{item[1] if item[1] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Area [2]
+                print(fixed_width(
+                    'Area:',
+                    f'{item[2] if item[2] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Vacancy [3]
+                print(fixed_width(
+                    'Vacancy:',
+                    f'{item[3] if item[3] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Salary from [4]
+                print(fixed_width(
+                    'Salary from:',
+                    f'{item[4] if item[4] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # URL [5]
+                print(fixed_width(
+                    'URL:',
+                    f'{item[5] if item[5] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                print('=' * TEXT_WIDTH)
 
         elif user_input.lower() == 'avgsal':
-            pass
+            avg_sal = db_manager.get_avg_salary()
+            print('=' * TEXT_WIDTH)
+
+            # TEMP
+            # print(avg_sal)
+
+            print(f'Average salary: {avg_sal}')
+            print(fixed_width(
+                'Average salary:',
+                f'{round(avg_sal[0][0]) if avg_sal[0][0] else '-Unknown-'}',
+                '-',
+                TEXT_WIDTH
+            ))
+
+            print('=' * TEXT_WIDTH)
 
         elif user_input.lower() == 'hiavg':
-            pass
+            higher_than_avg_vacancies = db_manager.get_vacancies_with_higher_salary()
+            print('~' * TEXT_WIDTH)
+            print('FOUND VACANCIES WITH SALARY THAT HIGHER THAN AVERAGE:'.center(TEXT_WIDTH, '~'))
+            print('~' * TEXT_WIDTH)
+            for item in higher_than_avg_vacancies:
+                print('=' * TEXT_WIDTH)
+
+                # TEMP
+                # print(item)
+
+                # ID [0]
+                print(fixed_width(
+                    'ID:',
+                    f'{item[0] if item[0] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Employer [1]
+                print(fixed_width(
+                    'Company:',
+                    f'{item[1] if item[1] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Area [2]
+                print(fixed_width(
+                    'Area:',
+                    f'{item[2] if item[2] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Vacancy [3]
+                print(fixed_width(
+                    'Vacancy:',
+                    f'{item[3] if item[3] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Salary from [4]
+                print(fixed_width(
+                    'Salary from:',
+                    f'{item[4] if item[4] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # URL [5]
+                print(fixed_width(
+                    'URL:',
+                    f'{item[5] if item[5] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                print('=' * TEXT_WIDTH)
 
         elif user_input.lower() == 'keyw':
-            pass
+            keyword = input('Enter your keyword: ')
+            keyw_vacancies = db_manager.get_vacancies_with_keyword(keyword)
+            print('~' * TEXT_WIDTH)
+            print('FOUND VACANCIES WITH YOUR KEYWORD:'.center(TEXT_WIDTH, '~'))
+            print('~' * TEXT_WIDTH)
+            for item in keyw_vacancies:
+                print('=' * TEXT_WIDTH)
+
+                # TEMP
+                print(item)
+
+                # ID [0]
+                print(fixed_width(
+                    'ID:',
+                    f'{item[0] if item[0] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Employer [1]
+                print(fixed_width(
+                    'Company:',
+                    f'{item[1] if item[1] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Area [2]
+                print(fixed_width(
+                    'Area:',
+                    f'{item[2] if item[2] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Vacancy [3]
+                print(fixed_width(
+                    'Vacancy:',
+                    f'{item[3] if item[3] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # Salary from [4]
+                print(fixed_width(
+                    'Salary from:',
+                    f'{item[4] if item[4] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                # URL [5]
+                print(fixed_width(
+                    'URL:',
+                    f'{item[5] if item[5] else '~Unknown~'}',
+                    '-',
+                    TEXT_WIDTH
+                ))
+
+                print('=' * TEXT_WIDTH)
         else:
             print('Your request is not provided, please enter another request')
 
