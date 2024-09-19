16c2b5591da6299089b52168144acf51b1ecc8da
georgii <goqwertys@gmail.com>
2024-09-19 20:09:42 +0400

    get_vacancies_with_keyword() added to DBManager() / some fixes at DBManager

--- src/db_manager.py
+++ src/db_manager.py
@@ -18,20 +18,20 @@ logger.addHandler(fh)
 class DBManager(ABSManager):
     """ Database manager """
 
-    def __init__(self, params=None):
+    def __init__(self, db_name, params=None):
         """ Connects to Database and creates cursor """
-        self.conn = psycopg2.connect(dbname='hh_db', **params)
+        self.conn = psycopg2.connect(dbname=db_name, **params)
         self.cur = self.conn.cursor()
 
     def get_companies_and_vacancies_count(self):
         """ Returns a list of all companies and the number of vacancies for each company"""
         self.cur.execute(
             """
-                SELECT employer_name, COUNT(vacancies.employer_id)
-                FROM employers
-                INNER JOIN vacancies USING (employer_id)
-                GROUP BY employer_name
-                ORDER BY COUNT DESC
+            SELECT employer_name, COUNT(vacancies.employer_id)
+            FROM employers
+            INNER JOIN vacancies USING (employer_id)
+            GROUP BY employer_name
+            ORDER BY COUNT DESC
             """
         )
         return self.cur.fetchall()
@@ -40,7 +40,7 @@ class DBManager(ABSManager):
         """ Returns a list of all vacancies with the company name, job title and salary, and a link to the vacancy."""
         self.cur.execute(
             """
-            SELECT e.name AS employer_name, v.vacancy_name, v.salary, a.name AS area_name, v.url
+            SELECT v.vacancy_id, e.employer_name, a.name AS area_name, v.vacancy_name, v.salary, v.vacancy_url
             FROM vacancies v
             INNER JOIN employers e USING (employer_id)
             INNER JOIN areas a ON v.vacancy_area = a.area_id
@@ -57,18 +57,39 @@ class DBManager(ABSManager):
             FROM vacancies
             """
         )
-        self.cur.fetchall()
+        return self.cur.fetchall()
 
     def get_vacancies_with_higher_salary(self):
         """ Returns a list of all vacancies where the salary is higher than the average for all vacancies """
         self.cur.execute(
             """
-            SELECT v.vacancy_name, v.salary
+            SELECT v.vacancy_id, e.employer_name, a.name AS area_name, v.vacancy_name, v.salary, v.vacancy_url
             FROM vacancies v
-            WHERE salary >= (SELECT AVG(salary) FROM vacancies)
+            INNER JOIN employers e USING (employer_id)
+            INNER JOIN areas a ON v.vacancy_area = a.area_id
+            WHERE v.salary >= (SELECT AVG(salary) FROM vacancies)
+            ORDER BY v.salary DESC
             """
         )
-        self.cur.fetchall()
+        return self.cur.fetchall()
 
     def get_vacancies_with_keyword(self, keyword: str):
         """ Returns a list of all vacancies whose titles contain the words passed to the method, for example python """
+        query = """
+            SELECT v.vacancy_id, e.employer_name, a.name AS area_name, v.vacancy_name, v.salary, v.vacancy_url
+            FROM vacancies v
+            INNER JOIN employers e USING (employer_id)
+            INNER JOIN areas a ON v.vacancy_area = a.area_id
+            WHERE v.salary >= (SELECT AVG(salary) FROM vacancies)
+            """
+
+        q_params = []
+
+        if keyword:
+            query += " AND (v.vacancy_name ILIKE %s OR e.employer_name ILIKE %s OR a.name ILIKE %s)"
+            q_params.extend([f"%{keyword}%"] * 3)
+
+        query += " ORDER BY v.salary DESC"
+
+        self.cur.execute(query, q_params)
+        return self.cur.fetchall()
